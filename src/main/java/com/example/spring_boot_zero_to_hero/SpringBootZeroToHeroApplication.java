f17ad7b97c14ba094f2c47b69a69931e53fd1146
kalyan <programmer143143@gmail.com>
2026-05-23 17:26:37 +0530

    spring boo tgetting started

---
+++ src/main/java/com/example/spring_boot_zero_to_hero/SpringBootZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_zero_to_hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZeroToHeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZeroToHeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_zero_to_hero/SpringBootZeroToHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_zero_to_hero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZeroToHeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
